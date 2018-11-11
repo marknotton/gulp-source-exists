7cc90f736d2dfaf7e3bc924a212b2a57fba530c7
Mark Notton <mark@yellostudio.co.uk>
2018-11-11 22:44:01 +0000

    Updated - CLI Styling

--- index.js
+++ index.js
@@ -7,21 +7,20 @@ const through2 = require('through2'),
       log      = require('fancy-log'),
       chalk    = require('chalk');
 
-module.exports = (files, fail = false, callback) => {
+module.exports = (files, callback = false) => {
 
   let filepaths = files.filter(file => !file.includes('*') && !file.includes('!'))
   let missing = filepaths.filter(file => fileExists(path.resolve(process.cwd(), file)) !== true);
 
   if (missing.length) {
     missing.forEach(file => {
-      if (fail) {
-        throw Error("The following file from your src stream doesn't exist: " +  missing)
-      }
 
-			let message = `${chalk.red("Missing file:")} ${chalk.red('Could not find '+file)}`;
+			let message = `${chalk.red("Missing:")} ${chalk.red('Could not find '+file)}`;
 
-			if ( typeof callback !== 'undefined') {
-				callback(message);
+      if (callback == true) {
+        throw Error(message)
+      } else if ( typeof callback == 'function') {
+				callback(`[${(new Date()).toTimeString().substr(0,8)}] ${message}`);
 			} else {
       	log(message);
 			}
